61da77db00fc07a27c66725fcc1652e3e6e06737
zmfkzj <qlwlal@naver.com>
2022-08-10 20:25:45 +0900

    test code 작성중...

--- src/main/java/com/example/intermediate/jwt/JwtFilter.java
+++ src/main/java/com/example/intermediate/jwt/JwtFilter.java
@@ -15,6 +15,7 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
--- src/main/java/com/example/intermediate/repository/RefreshTokenRepository.java
+++ src/main/java/com/example/intermediate/repository/RefreshTokenRepository.java
@@ -3,6 +3,7 @@ package com.example.intermediate.repository;
 import com.example.intermediate.domain.Member;
 import com.example.intermediate.domain.RefreshToken;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
